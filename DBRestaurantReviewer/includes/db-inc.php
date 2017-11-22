e739ebab8336c865a589aaf14344a37ed4c00ccb
padalah <padalah@oregonstate.edu>
2017-11-22 01:07:18 -0800

    Added to Sathya's DB

--- DBRestaurantReviewer/includes/db-inc.php
+++ DBRestaurantReviewer/includes/db-inc.php
@@ -1,9 +1,9 @@
 <?php
   // Define database connection constants
   define('DB_HOST', 'classmysql.engr.oregonstate.edu');
-  define('DB_USER', 'cs340_barver');
-  define('DB_PASSWORD', '6463');
-  define('DB_NAME', 'cs340_barver');
+  define('DB_USER', 'cs340_ramanats');
+  define('DB_PASSWORD', '2073');
+  define('DB_NAME', 'cs340_ramanats');
 
   $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
   if (!$conn) {
